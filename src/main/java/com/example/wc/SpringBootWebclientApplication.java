2fd4e372b8a441e507e48827fe3379eb7ca1de52
aysenurgkdmr <agokdemir@efatura.altair.com.tr>
2022-01-22 12:54:18 +0300

    Initial commit

---
+++ src/main/java/com/example/wc/SpringBootWebclientApplication.java
@@ -0,0 +1,13 @@
+package com.example.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebclientApplication.class, args);
+	}
+
+}
